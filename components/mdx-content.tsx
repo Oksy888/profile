e6dcf5a6da470638b60420192cf7f3ef26ecc8d9
Oksy888 <103371949+Oksy888@users.noreply.github.com>
2025-01-05 19:01:27 +0200

    refactor Code component to use TypeScript for props definition and improve type safety

--- components/mdx-content.tsx
+++ components/mdx-content.tsx
@@ -4,8 +4,12 @@ import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote/rsc'
 
 import Counter from '@/components/counter'
 
-function Code({ children, ...props }: any) {
-  let codeHTML = highlight(children)
+type CodeProps = React.HTMLAttributes<HTMLElement> & {
+  children: string
+}
+
+function Code({ children, ...props }: CodeProps) {
+  const codeHTML = highlight(children)
   return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />
 }
 
